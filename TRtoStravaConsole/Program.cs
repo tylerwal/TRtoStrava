e7694fc66533b85aca8bfc632ea529d6665a4413
Tyler Waldron <tylerwal@gmail.com>
2015-01-22 22:59:05 -0500

    Adding an export of a successful api request

--- TRtoStravaConsole/Program.cs
+++ TRtoStravaConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,15 @@ namespace TRtoStravaConsole
 	{
 		static void Main(string[] args)
 		{
+			WebClient wc = new WebClient();
+
+			string page = wc.DownloadString(@"http://www.trainerroad.com/career/tylerwal");
+
+			File.WriteAllText(@"C:\page.html", page);
+
+			//Console.Write(page);
+
+			//Console.ReadLine();
 		}
 	}
 }
